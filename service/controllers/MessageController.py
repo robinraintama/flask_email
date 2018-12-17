91bfb4b970f571ef1379f136d825301a5c981673
robinraintama <robinraintama@gmail.com>
2018-12-17 11:48:52 +0700

    Fix Send Email - not done
    todo:
    - Fix Send Email
    - Implement package
    - Implement supervisor
    - Save send emails as transaction
    - Add db model for send email data
    - Review
    - Build GUI?

--- service/controllers/MessageController.py
+++ service/controllers/MessageController.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
-from flask import requests
+from flask import request
 
 from service.schemas import MessageSchema, EmailSchema
 from service.models import tr_messages, tm_emails
@@ -22,17 +22,19 @@ def getEmails():
 
 def getMessageAtTimestamp():
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M')
-    messages = tr_messages.Messages.query.filter_by(timestamp=timestamp).all()
+#     messages = tr_messages.Messages.query.filter_by(timestamp=timestamp).all()
+    messages = tr_messages.Messages.query.all()
     return MessageSchema.all_message_schema.dump(messages).data
 
 def sendEmail(email_address, subject, message):
-    return requests.post(
+    return request.post(
         "https://api.mailgun.net/v3/{}/messages".format(app.config['MAIL_DOMAIN']),
-        auth=("api", app.config['MAIL_KEY']),
-        data={"from": "Robin Raintama <mailgun@{}}>".format(app.config['MAIL_DOMAIN']),
+        auth=("api", app.config['MAIL_API_KEY']),
+        data={"from": "{}} <mailgun@{}}>".format(app.config['MAIL_SENDER'], app.config['MAIL_DOMAIN']),
               "to": [email_address],
               "subject": subject,
-              "text": message})
+              "text": message},
+        timeout = (5.0, 30.0))
 
 def convertToDateTime(str):
     return datetime.strptime(str, '%d %b %Y %H:%M')
--- service/tasks/task_email.py
+++ service/tasks/task_email.py
@@ -1,5 +1,7 @@
 import logging
 import celery
+import app
+from flask import request
 from service.controllers import MessageController
 
 @celery.task()
@@ -11,11 +13,24 @@ def email_users():
     logger.info(messages)
     logger.info(emails)
 
+    connect_timeout, read_timeout = 5.0, 30.0
+
     try:
-        for message in message:
-            for user in users:
-                MessageController.sendEmail(user.email_address, message.subject, message.message)
+        for message in messages:
+            for mail in emails:
+                with MessageController.sendEmail(mail['email'], message['email_subject'], message['email_content']):
+                    logger.info("Success {}".format(mail['email']))
     
     except Exception as e:
         logging.exception(e)
         logger.info(e)
+
+def sendEmail(email_address, subject, message):
+    return request.post(
+        "https://api.mailgun.net/v3/{}/messages".format(app.config['MAIL_DOMAIN']),
+        auth=("api", app.config['MAIL_API_KEY']),
+        data={"from": "{}} <mailgun@{}}>".format(app.config['MAIL_SENDER'], app.config['MAIL_DOMAIN']),
+              "to": [email_address],
+              "subject": subject,
+              "text": message},
+        timeout = (5.0, 30.0))
