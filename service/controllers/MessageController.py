ad5a387923430bedbf0a454ee4dfbba84368a639
robinraintama <robinraintama@gmail.com>
2018-12-20 21:32:24 +0700

    Add delay function
    - calculate delay from timestamp difference
    
    todo:
    - gui
    - more test

--- service/controllers/MessageController.py
+++ service/controllers/MessageController.py
@@ -11,17 +11,21 @@ from service.tasks.task_email import email_users
 def saveMessage(data):
     try:
         data['timestamp'] = convertToDateTime(data['timestamp'])
-        message = tr_messages.Messages(**data)
-        db.session.add(message)
+        if data['timestamp'] > datetime.now():
+                delaySeconds = getSecondsDifference(data['timestamp'])
+                message = tr_messages.Messages(**data)
+                db.session.add(message)
 
-        arEmails = list()
-        emails = getEmails()
-        for email in emails:
-                arEmails.append(email['email'])
-        print(arEmails, data["email_subject"], data["email_content"]) 
-        email_users.apply_async(args=[arEmails, data["email_subject"], data["email_content"]], countdown=1)
+                arEmails = list()
+                emails = getEmails()
+                for email in emails:
+                        arEmails.append(email['email'])
+                print(arEmails, data["email_subject"], data["email_content"]) 
+                email_users.apply_async(args=[arEmails, data["email_subject"], data["email_content"]], countdown=delaySeconds)
 
-        return db.session.commit()
+                return db.session.commit()
+        else:
+                return 'Check your datetime'
     except Exception as e:
         logging.exception(e)
         return 'Please check your request'
@@ -35,6 +39,9 @@ def getMessageAtTimestamp():
 #     messages = tr_messages.Messages.query.filter_by(timestamp=timestamp).all()
     messages = tr_messages.Messages.query.all()
     return MessageSchema.all_message_schema.dump(messages).data
+
+def getSecondsDifference(dt):
+        return (dt - datetime.now().timestamp())
 
 def sendEmail(email_addresses, subject, message):
         print(email_addresses, subject, message)
